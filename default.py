a5fe787b0acf0a43c647fb628f26b2c4e7624e56
Rmolimock <495@holbertonschool.com>
2020-06-19 07:19:01 +0000

    transfer of auths broken down into separate methods for use cases

--- default.py
+++ default.py
@@ -1,247 +1,256 @@
 #!/usr/bin/env python3
+from uuid import uuid4
+from typing import List
+
+
 class Requirement():
-    """ Requirement class """
+    """
+    Requirements Class
+    """
     def __init__(self):
+        self.id = uuid4()
         self.__conditions = []
         self.__operator = ''
     @property
-    def operator(self):
-        return self.__operator
-    @property
     def conditions(self):
         return self.__conditions
-    @conditions.setter
-    def conditions(self, value):
-        if not type(value) == str or type(value) == list:
+    @property
+    def operator(self):
+        return self.__operator
+    def setup(self, operator: str, conditions: List) -> bool:
+        """
+        Instantiate the operator (str) and conditions (list).
+        """
+        if not type(operator) == str or not conditions == list:
             return False
-        self.__conditions = value
-    @operator.setter
-    def operator(self, value):
-        if not type(value) == str:
+        allowed_operators = ['and', 'or']
+        allowed_conditions = ['1 == 1', '1 == 2']
+        if not operator in allowed_operators:
             return False
-        self.__operator = value
-    def create(self, operator, conditions):
-        """ create a single rule """
-        if not type(operator) == str:
-            print('operator must be a string')
-            return False
-        if not type(conditions) == list:
-            if not type(conditions) == Requirement:
-                if not type(conditions) == str:
-                    print('conditions must be strings or Requirement objects')
-                    return False
-            self.__conditions = [conditions]
-            self.__operator = operator
-            return True
-        for c in conditions:
-            if not type(c) == str and not type(c) == Requirement:
-                print('conditions must be strings or Requirement objects')
+        for each in conditions:
+            if not type(each) == str and not type(each) == Requirement:
                 return False
-        self.__conditions = conditions
+            if each not in allowed_conditions:
+                return False
         self.__operator = operator
-    def validate(self, auth):
-        """ validate all requirements for action """
-        if self.operator == 'and':
-            return self.and_operator(auth)
-        if self.operator == 'or':
-            return self.or_operator(auth)
-    def and_operator(self, auth):
-        member = auth['member']
-        subject = auth['subject']
-        action = auth['action']
-        parameters = auth['parameters']
+        self.__conditions = conditions
+        return True
+    def validate(self, current_parameters: dict) -> bool:
+        """
+        Determine if the requirement is valid.
+        """
+        return eval(self.operator + '_operator(current_parameters')
+    def and_operator(self, current_parameters):
+        """
+        Validates true if all conditions are true.
+        """
+        p = current_parameters
+        weilded_by = p.get('weilded_by')
+        over_whom = p.get('over_whom')
+        grantors = p.get('grantors')
+        original_grantor = p.get('original_grantor')
+        action = p.get('action')
+        requirement = self
+        parameters = p('parameters')
         for condition in self.conditions:
             if type(condition) == str:
-                    # something like:
-                    #   'member.uid == 123'
-                    # or:
-                    #   '"arg" in parameters'
-                    #   'parameters[arg] == 987
                 if not eval(condition):
                     return False
             elif type(condition) == Requirement:
-                if not condition.validate(auth):
+                if not condition.validate(p):
                     return False
         return True
-    def or_operator(self, auth):
-        member = auth['member']
-        subject = auth['subject']
-        action = auth['action']
-        parameters = auth['parameters']
+    def or_operator(self, current_parameters):
+        """
+        Validates true if any conditions are true.
+        """
+        p = current_parameters
+        weilded_by = p.get('weilded_by')
+        over_whom = p.get('over_whom')
+        grantors = p.get('grantors')
+        original_grantor = p.get('original_grantor')
+        action = p.get('action')
+        requirement = self
+        parameters = p('parameters')
         for condition in self.conditions:
             if type(condition) == str:
                 if eval(condition):
                     return True
             elif type(condition) == Requirement:
-                if condition.validate(auth):
+                if condition.validate(p):
                     return True
-            return False
+        return False
+        
 
+class Authority():
+    """
+    Authority Class
+    """
+    def __init__(self, original_grantor):
+        self.id = uuid4()
+        self.weilded_by = []
+        self.over_whom = []
+        self.grantors = []
+        self.__original_grantor = original_grantor
+        self.action = ''
+        self.granted_action = ''
+        self.requirement = Requirement()
+    @property
+    def original_grantor(self):
+        return self.__original_grantor
+    
 
-
-
+Authorities = [Authority()]
 
 
 class Member():
+    """
+    Member Class
+    """
     def __init__(self, name):
-        from uuid import uuid4
-        self.uid = str(uuid4())
         self.name = name
-        self.teams = {}
-        self.roles = {}
-        self.subject_to = {True: {'example_action': {False: False}}}
-        self.authority_over = {self.uid: {'example_action': {True: True, '__transferable__': False}},
-                               None: {'example_action': {False: False}}
-                               }
-    # simple get/print/format helper methods
-    def example_action(self):
-        """ empty method """
-        return
-    def get_auth_by_uid(self, uid):
-        """
-        return the actions and their requirements by given uid
-        """
-        if uid not in self.authority_over:
-            return {}
-        return self.authority_over[uid]
-    def get_auth_by_action(self, action):
-        """
-        return uids the member has authority to perform a given action on
-        """
-        uids = {}
-        for key, action_dict in self.authority_over.items():
-            if action in action_dict:
-                uids[key] = {action: {}}
-                for r in action_dict[action]:
-                    uids[key][action][r] = r
-        return uids
-    def print_authorities(self):
-        """
-        print a representation of the member's authorities
-        """
-        for s in self.authority_over:
-            print('uid:')
-            print("    {}".format(s))
-            print()
-            for a in self.authority_over[s]:
-                print('    ACTION:')
-                print("        {}".format(a))
-                print()
-                for r in self.authority_over[s][a]:
-                    print('        REQUIREMENT:')
-                    print("            {}".format(r))
-        return True
-    def format_action(self, action):
-        """
-        format action from obj.method() to obj and method
-        """
-        # action
-        if '.' in action:
-            action = action.split('.')[1]
-        while action.endswith('()') or action.endswith('(') or action.endswith(')'):
-            while action.endswith('()'):
-                action = action[:-2]
-            while action.endswith('(') or action.endswith(')'):
-                action = action[:-1]
-        return action
-    # methods that handle Member authorities
-    def has_authority(self, action, uid, parameters=None):
-        """
-        check if member has the authority to perform an action on the given
-        uid, and if all requirements are met.
-        uid: entity on which member is peforming the action
-        action: the action being performed on uid
-        """
-        if not type(action) == str:
-            raise TypeError('action must be a string, ex: "user.method" ')
-        action = self.format_action(action)
-        try:
-            # check if action is a valuid function for member
-            # eval should be safe here because format_action removes any () 
-            eval('self.' + action)
-        except AttributeError:
-            print(self.name + ' does not have that method')
+        self.id = uuid4()
+    def grant_authority_over_self(self, action, weilded_by, requirement):
+        if not self.has_authority_grant_from_self(action):
             return False
-        except Exception as e:
-            raise e
-        if uid == self:
-            return action
-        if uid not in self.authority_over:
+        for auth in Authorities:
+            if (auth.action == action and self in auth.over_whom
+               and requirement.is_same(auth.requirement)):
+                for each in weilded_by:
+                   auth.weilded_by.append(each)
+                return True
+        auth = Authority(self.id)
+        auth.action = action
+        auth.grantors = [self.id]
+        auth.over_whom = self.id
+        auth.requirement = requirement
+        auth.weilded_by = []
+        del auth.granted_action
+        for each in weilded_by:
+            auth.weilded_by.append(each)
+    def same_weilded_by_and_over_whom(self, auth, weilded_by, over_whom):
+        
+    def grant_meta_authority_to_others(self, action, weilded_by, over_whom, requirement):
+        if not self.has_authority_grant_from_self(action):
             return False
-        if action not in self.authority_over[uid]:
+        for auth in Authorities:
+            if (auth.action == 'grant_authority'
+               and auth.granted_action == action
+               and requirement.is_same(auth.requirement)):
+                new_weilded_by = False
+                missing_weilded_by = False
+                new_overs = False
+                missing_overs = False
+                for new in weilded_by:
+                    if not new in auth.weilded_by:
+                        new_weilded_by = True
+                for existing in auth.weilded_by:
+                    if not existing in weilded_by:
+                        missing_weilded_by = True
+                for new in over_whom:
+                    if not new in auth.over_whom:
+                        new_overs = True
+                for existing in auth.over_whom:
+                    if not existing in over_whom:
+                        missing_overs = True
+                if new_weilded_by and not new_overs and not missing_overs:
+                    for each in weilded_by:
+                        auth.weilded_by.append(each)
+                    return auth
+                elif new_overs and not new_weilded_by and not missing_weilded_by:
+                    for each in over_whom:
+                        auth.over_whom.append(each)
+                    return auth
+        auth = Authority(self.id)
+        auth.action = 'grant_authority'
+        auth.granted_action = action
+        auth.grantors = [self.id]
+        auth.requirement = requirement
+        auth.over_whom = []
+        for each in over_whom:
+            auth.over_whom.append(each)
+        auth.weilded_by = []
+        for each in weilded_by:
+            auth.weilded_by.append(each)
+        Authorities.append(auth)
+        return auth
+    def grant_authority_over_other(self, action, weilded_by, over_whom, requirement):
+        if not self.has_authority_grant_other(action, weilded_by, over_whom):
             return False
-        requirement = self.authority_over[uid][action]
-        auth = {'member': self, 
-                 'subject': uid,
-                 'action': action,
-                 'parameters': parameters}
-        if not requirement.validate(auth):
+        for auth in Authorities:
+            if (auth.action == action
+               and requirement.is_same(auth.requirement)
+               and over_whom == auth.original_grantor):
+                for each in weilded_by:
+                    if not each in auth.weilded_by:
+                        auth.weilded_by.append(each)
+                    return auth
+        auth = Authority(self.id)
+        auth.action = action
+        auth.grantors = [over_whom, self.id]
+        auth.original_grantor = over_whom
+        auth.requirement = requirement
+        auth.over_whom = over_whom
+        auth.weilded_by = []
+        for each in weilded_by:
+            auth.weilded_by.append(each)
+        Authorities.append(auth)
+        return auth
+
+
+
+                   
+    def grant_authority_over_other(self, granted_action, weilded_by, requirement):
+        if not self.has_authority_grant_from_other():
             return False
-        return action
-    def grant_authority(self, action, subject, requirements=False):
+    def has_authority_grant_from_self(self, action):
         """
-        member yields an authority over themselves to another entity
+        Verify action is a method of class self.
         """
-        # HEY create optional giver parameter for being more explicit about
-        # over whom the authority is that's transfering from giver->reciever.
-        # WHAT ABOU PARAMETERS*************
-        # FIX uid so it's the uid you're giving the auth to!
-        # one possibility for dealing with transferable authorities is
-        # to have a special __transferable__ key in self.authority_over[uid]
-        # where uid is not self. This would have it's own requirements just
-        # like an action key would, meaning the authority is tranferable so long
-        # as given requirements evaluate true
-        # rework this so you create the dict (or sub dict) first then enter it into
-        # subject.auth_over at the end, same time enter uuid into self.subject_to
-        # change uid in THIS METHOD ONLY to subject
-        # verify member has the authority they're trying to grant to other
-        # member, and that it's transferable
-        # you'll have to rework has authority to handle transfering auths from other members
-        action = self.has_authority(action, self)
-        if not action:
-            return False
-        uid_auth = subject.get_auth_by_uid(self)
-        if not uid_auth:
-            subject.authority_over[self.uid] = {action: {}}
-        elif not action in uid_auth:
-            subject.authority_over[self.uid][action] = {}
-        if requirements:
-            for r in requirements:
-                subject.authority_over[self.uid][action][r] = r
-        else:
-            subject.authority_over[self.uid][action][True] = True
-        if not subject.uid in self.subject_to:
-            self.subject_to[subject.uid] = {action: {}}
-        else:
-            self.subject_to[subject.uid][action] = {}
-        if requirements:
-            for r in requirements:
-                self.subject_to[subject.uid][action][r] = r
-        else:
-            self.subject_to[subject.uid][action][True] = True
-        return True
-    def relinquish_authority(self, action, uid):
+        return True if action in self.__class__.__dict__ else False
+    def has_authority_grant_other(self, action, weilded_by, requirement):
         """
-        relinquish an authority that Member already has where action is entity.action
+        Verify self has a matching authority.
         """
-        action = self.has_authority(action, uid)
-        if not action:
-            return False
-        uid, action = self.format_action(action)
-        if uid == self:
-            raise('self ownership is immutable')
-        if not uid in self.authority_over:
-            return False
-        if action and action not in self.authority_over[uid]:
-            return False
-        if not action:
-            del self.authority_over[uid]
-            return True
-        del self.authority_over[uid][action]
-        """ next up is revoke_authority(): """
-        return True
+        p = {'action': action, 'weilded_by': weilded_by,
+             'requirement': requirement, 'over_whom': over_whom}
+        for auth in Authorities:
+            if (self in auth.weilded_by and over_whom == auth.over_whom
+               and 'grant_authority' == auth.action
+               and action == auth.granted_action):
+                if auth.requirement.validate(p):
+                    return True
+        return False
 
 
 
 
 
+"""
+        Grant a currently held authority to another Member, Team, or Role.
+        1. If over_whom != self:
+            MEMBER B TRANSFERS AUTH TO DO X OVER MEMBER A TO MEMBER C
+            a. create current_parameters dictionary from above parameters.
+            b. if action == 'grant_authority':
+                Confirm there is an authority where:
+                    * auth.action == 'grant_authority'
+                    * self is in auth.weilded_by
+                    * auth.original_grantor == c
+                    * over_whom is in auth.over_whom
+                    * auth.requirement.validate(p) == True
+            c. if action != 'grant_authority':
+                Confirm there is an authority where:
+                    * auth.action == action
+                    * self in auth.weilded_by
+                    * over_whom in auth.over_whom
+                    * over_whom in auth.original_grantor
+                    * auth.requirement.validate(p) == True
+        2. If over_whom == self:
+            MEMBER A GRANTS B THE AUTH TO DO X ON A
+            Check if A has authority to do X.
+                If over_whom == self:
+                    Confirm eval(action) is a method of self.class.dict
+                    Member A has authority
+                If auth.over_whom != self:
+                    check_authority()
+"""
