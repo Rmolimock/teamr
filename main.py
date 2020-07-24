4c0d4025a1150b9c827eaad265357c2a9f9543a2
Rmolimock <495@holbertonschool.com>
2020-07-23 23:39:37 -0500

    refactoring authentication

--- main.py
+++ main.py
@@ -20,11 +20,60 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
+
+public_route = ['/api/v1/status/',
+                '/api/v1/unauthorized/',
+                '/api/v1/forbidden/',
+                '/api/v1/views/login/',
+                '/auth/login',
+                '/auth/login.html',
+                '/login',
+                '/logout',
+                '/register',
+                '/reset_password',
+                '/reset_password/*',
+                '/static/*',
+                '/users',
+                '/users/*',
+                '/teams',
+                '/teams/*']
+
 # create the pymongo client and connect to the teamr database
 
 
 # instantiate the session authentication object
 auth = Auth()
+
+def authenticate_current_user(request):
+    """
+    ----------------------------
+    Check if current session is associated with a user.
+    ----------------------------
+    -> Return: True or abort with approriate errorhandler.
+    """
+    session = auth.session_exists(request)
+    if not session:
+        # no session cookie is present
+        if auth.require_auth(request.path, public_route):
+            # page requested requires authentication
+            print('authentication required!')
+            abort(401)
+        print('good')
+        return
+    # a session cookie is present
+    current_user = auth.current_user(session)
+    if not current_user:
+        # session cookie is invalid
+        if auth.require_auth(request.path, public_route):
+            print('authentication required!')
+            abort(401)
+        print('good t')
+        return
+    # a session cookie is present and valid
+    from models import User
+    request.current_user = User.get(current_user)
+    print('good to go')
 
 
 
@@ -39,39 +88,12 @@ def before():
     -> Return: None or abort with approriate errorhandler.
     """
     from models.user import User
-    users = User.search()
-    for each in users:
-        print(each.id, each.email)
+    request.current_user = None
     now = datetime.now()
-    if now.hour == 0:
-        delete_expired_sessions()
+    delete_expired_sessions()
     if auth:
-        if not auth.require_auth(
-                            request.path,
-                            ['/api/v1/status/',
-                             '/api/v1/unauthorized/',
-                             '/api/v1/forbidden/',
-                             '/api/v1/views/login/',
-                             '/auth/login',
-                             '/auth/login.html',
-                             '/login',
-                             '/logout',
-                             '/register',
-                             '/reset_password',
-                             '/reset_password/*',
-                             '/static/*',
-                             '/users',
-                             '/users/*']):
-            return None
-        session = auth.session_cookie(request)
-        if not session:
-            abort(401)
-        print('in auth')
-        current_user = auth.current_user(session)
-        if not current_user:
-            abort(403)
-        from models import User
-        request.current_user = User.get(current_user)
+        authenticate_current_user(request)
+        return None
 
 
 def delete_expired_sessions():
@@ -85,7 +107,6 @@ def delete_expired_sessions():
         start = v[1]
         print(k)
         if datetime.now() > start + timedelta(seconds=auth.session_duration):
-            print('deleted session')
             del Auth.session_ids[k]
 
 # configure what responses various abort() calls generate
