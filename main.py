7112c96840601b79610edd76cecaf5df16895332
Rmolimock <495@holbertonschool.com>
2020-07-26 20:19:19 +0100

    complete authentication overhaul using flask-login

--- main.py
+++ main.py
@@ -3,113 +3,245 @@
 Route module for the API
 """
 # this is the new one
-from api.v1.views.auth.auth import Auth
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request, redirect, render_template
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
 from flask_cors import (CORS, cross_origin)
+from flask import jsonify, Flask, render_template, request, redirect, url_for, flash
+from flask_login import login_required, current_user, LoginManager, login_user, logout_user
+from models import User, db
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
-from models import db
-from datetime import datetime, timedelta
+# from api.v1.views import app_views
+
 
 
 
 # instantiate the app, add cors protection, and check which authentication
 # method it should run with.
 app = Flask(__name__)
-app.register_blueprint(app_views)
+# app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+import secrets
+app.secret_key = os.environ.get("FLASK_SECRET_KEY")
+print(app.secret_key)
 
-
-
-public_route = ['/api/v1/status/',
-                '/api/v1/unauthorized/',
-                '/api/v1/forbidden/',
-                '/api/v1/views/login/',
-                '/auth/login',
-                '/auth/login.html',
-                '/login',
-                '/logout',
-                '/register',
-                '/reset_password',
-                '/reset_password/*',
-                '/static/*',
-                '/users',
-                '/users/*',
-                '/teams',
-                '/teams/*']
-
-# create the pymongo client and connect to the teamr database
-
-
-# instantiate the session authentication object
-auth = Auth()
-
-def authenticate_current_user(request):
+"""
+----------------------------
+Flask-login setup
+----------------------------
+"""
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+# login_manager.login_view = redirect on bad login
+@login_manager.user_loader
+def load_user(user_id):
     """
-    ----------------------------
-    Check if current session is associated with a user.
-    ----------------------------
-    -> Return: True or abort with approriate errorhandler.
+    Tell Flask-Login how to find a specific user
+    from the ID that is stored in the session cookie.
     """
-    session = auth.session_exists(request)
-    if not session:
-        # no session cookie is present
-        if auth.require_auth(request.path, public_route):
-            # page requested requires authentication
-            print('authentication required!')
-            abort(401)
-        print('good')
-        return
-    # a session cookie is present
-    current_user = auth.current_user(session)
-    if not current_user:
-        # session cookie is invalid
-        if auth.require_auth(request.path, public_route):
-            print('authentication required!')
-            abort(401)
-        print('good t')
-        return
-    # a session cookie is present and valid
-    from models import User
-    request.current_user = User.get(current_user)
-    print('good to go')
+    return User.get(user_id)
 
 
+"""
+----------------------------
+Authentication Routes
+----------------------------
+"""
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# if route requested needs authentication, authenticate user
-@app.before_request
-def before():
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', user=current_user)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('auth/register.html')
+    # post request
+    from validate_email import validate_email
+    # get info from the registration form
+    email = request.form.get('email')
+    password = request.form.get('password')
+    username = request.form.get('username')
+    # check if the email is already in use
+    existing_user = User.search({'email': email})
+    if existing_user:
+        print('email in use')
+        flash('Email is already in use.')
+        return redirect('/register')
+    # check if the password is strong enough
+    if not password_check(password)[0]:
+        password_error = ''
+        for k,v in password_check(password)[1].items():
+            if v:
+                password_error += k
+        flash(password_error)
+        return render_template('./auth/register.html')
+    response = bad_uploaded_file(request)
+    if response:
+        print('file not ok')
+        flash('Profile picture must be one of these formats: .png, .jpg, .jpeg, or .bmp')
+        return render_template('./auth/register.html')
+    user_info = {
+                'email': email,
+                'password': generate_password_hash(password, method='sha256'),
+                'username': username
+                }
+    user = User(**user_info)
+    user.save_to_db()
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('auth/login.html')
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+    user = User.search({'email': email})
+    user = user[0] if user else None
+    if not user or not check_password_hash(user.password, password):
+        return redirect(url_for('login'))
+    login_user(user)
+    return redirect(url_for('profile'))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+"""
+----------------------------
+Helper functions.
+----------------------------
+"""
+def bad_uploaded_file(request):
+    image_formats = ['png', 'jpg', 'jpeg', 'bmp']
+    if 'file' not in request.files:
+        print(1)
+        return True
+    file = request.files['file']
+    if file.filename == '' or not '.' in file.filename:
+        print(2)
+        return True
+    if file.filename.split('.')[-1:][0] not in image_formats:
+        print(3)
+        print(file.filename.split('.')[-1:][0])
+        return True
+    return False
+
+def password_check(password):
     """
-    ----------------------------
-    Check if requested route is public.
-    If not, authenticate the user.
-    ----------------------------
-    -> Return: None or abort with approriate errorhandler.
+    Verify the strength of 'password'
+    Returns a dict indicating the incorrect criteria
+    A password is considered strong if:
+        8 characters length or more
+        1 digit or more
+        1 symbol or more
+        1 uppercase letter or more
+        1 lowercase letter or more
     """
-    from models.user import User
-    request.current_user = None
-    now = datetime.now()
-    delete_expired_sessions()
-    if auth:
-        authenticate_current_user(request)
-        return None
+    import re
+    # checking the length
+    length_error = len(password) < 8
+    # searching for digits
+    digit_error = re.search(r"\d", password) is None
+    # searching for uppercase
+    uppercase_error = re.search(r"[A-Z]", password) is None
+    # searching for lowercase
+    lowercase_error = re.search(r"[a-z]", password) is None
+    # searching for symbols
+    symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
+    # overall result
+    password_ok = not ( length_error or digit_error or uppercase_error or lowercase_error or symbol_error )
+    return (password_ok,
+            {
+            'password ': 1,
+            'is too short, ' : length_error,
+            'must contain a number, ' : digit_error,
+            'must contain an uppercase letter, ' : uppercase_error,
+            'must contain a lowercase letter, ' : lowercase_error,
+            'must contain a symbol.' : symbol_error,
+            })
+        
+
+@app.route('/register_old', methods=['GET', 'POST'])
+def register_old():
+    if request.method == 'GET':
+        print('request is get')
+        return render_template('./auth/register.html')
+    if request.method == 'POST':
+        print('request is post')
+        
+        file = request.files['file']
+        user_name = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = db.register(user_name, email, password)
+        if not type(user) == User or not 1:
+            print('error message')
+            return render_template('./auth/register.html')
+        login_user(user)
+        print('registered')
+        return render_template('./index.html')
+
+@app.route('/tokensignin', methods=['POST'], strict_slashes=False)
+def google_signin_token():
+    token = request.form.getlist('idtoken')[0]
+    from google.oauth2 import id_token
+    from google.auth.transport import requests
+    # (Receive token by HTTPS POST)
+    # ...
+    try:
+        
+        # Specify the CLIENT_ID of the app that accesses the backend:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+
+        # Or, if multiple clients access the backend server:
+        # idinfo = id_token.verify_oauth2_token(token, requests.Request())
+        # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+        #     raise ValueError('Could not verify audience.')
+
+        # If auth request is from a G Suite domain:
+        # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+        #     raise ValueError('Wrong hosted domain.')
+
+        # ID token is valid. Get the user's Google Account ID from the decoded token.
+        userid = idinfo['sub']
+        print('userid = ', userid)
+    except ValueError:
+        # Invalid token
+        print('invalid')
+        pass
+    return redirect('/users')
 
 
-def delete_expired_sessions():
-    """
-        ----------------------------
-        Occasionally delete expired sessions from the db and memory.
-        ----------------------------
-        -> Return: None.
-        """
-    for k, v in auth.session_ids.items():
-        start = v[1]
-        print(k)
-        if datetime.now() > start + timedelta(seconds=auth.session_duration):
-            del Auth.session_ids[k]
 
-# configure what responses various abort() calls generate
+
+
+
+
+
+
+
+
+"""
+----------------------------
+Error responses
+----------------------------
+"""
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -133,7 +265,6 @@ def Unauthorized(error) -> str:
 
 if __name__ == "__main__":
     # set host and port #s based on environmental variables and run app
-    host = getenv("API_HOST", "127.0.0.1")
-    port = getenv("API_PORT", "8080")
-    app.run(host=host, port=port, debug=True)
-
+    host = os.getenv("API_HOST", "127.0.0.1")
+    port = os.getenv("API_PORT", "8080")
+    app.run(host=host, port=port)
