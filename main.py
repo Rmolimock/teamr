3f192305699a47435937bc8334c0e7450b4b2347
Rmolimock <495@holbertonschool.com>
2020-07-27 20:53:25 -0500

    register users with google authenticate

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from models import User, db
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 from models.nav_link import *
+from validate_email import validate_email
+
 # from api.v1.views import app_views
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
 
@@ -56,69 +58,158 @@ Authentication Routes
 """
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # always render index.html with GOOGLE_CLIENT_ID
+    return render_template('index.html', GOOGLE_CLIENT_ID=GOOGLE_CLIENT_ID)
 
 @app.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', user=current_user)
+    print(current_user.image)
+    print(current_user.url)
+    return render_template('profile2.html', user=current_user, image=current_user.image)
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():
+@app.route('/register/<token>', methods=['GET', 'POST'])
+def register(token=None):
     if request.method == 'GET':
-        register_buttons[0].url = url_for('login')
         return render_template('auth/register.html',
                                 nav_buttons=register_buttons)
     # post request
-    from validate_email import validate_email
-    # get info from the registration form
-    email = request.form.get('email')
-    password = request.form.get('password')
-    username = request.form.get('username')
+    image_url = None
+    if token:
+        print(token)
+        guser = google_auth(request)
+        if guser[0]:
+            email = guser[1]['email']
+            username = guser[1]['name']
+            password = guser[1]['sub']
+            image_url = guser[1]['picture']
+            print(1)
+        else:
+            print(2)
+            return guser[1]
+    else:
+        # get info from the registration form
+        print(3)
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
     # check if the email is already in use
-    existing_user = User.search({'email': email})
-    if existing_user:
-        print('Email is already in use.')
-        flash('Email is already in use.')
+    print(4)
+    if validate_email(username):
+        flash('Username can not contain special characters.')
+        print(5)
+        return redirect('/register')
+    if not validate_email(email):
+        flash('Email must be valid.')
+        print(6)
         return redirect('/register')
     # check if the password is strong enough
-    if not password_check(password)[0]:
+    if not password_check(password)[0] and not token:
         password_error = ''
         for k,v in password_check(password)[1].items():
             if v:
                 password_error += k
         flash(password_error)
+        print(7)
         return redirect('/register')
-    response = bad_uploaded_file(request)
-    if response:
-        print('file not ok')
+    existing_user = User.search({'email': email})
+    if existing_user:
+        print('Email is already in use.')
+        flash('Email is already in use.')
+        print(8)
         return redirect('/register')
+    # check the format of profile picture
+    if not image_url:
+        response = bad_uploaded_file(request)
+        if response:
+            print('file not ok')
+            # flash happens in bad_uploaded_file()
+            print(9)
+            return redirect('/register')
     user_info = {
                 'email': email,
                 'password': generate_password_hash(password, method='sha256'),
                 'username': username
                 }
     user = User(**user_info)
-    save_profile_picture(file, user)
+    if not image_url:
+        print('imageurl:', image_url)
+        save_profile_picture(file, user)
+    else:
+        print('no image! Whaaaaaa!?')
+        user.image = image_url
+    print('user image set to: ', user.image)
+    print('user url set to ', user.url)
     user.save_to_db()
     flash(f'Welcome {username}!')
     login_user(user)
+    print(10)
     return redirect(url_for('profile'))
+
+
+
+def google_auth(request):
+    token = request.form.getlist('idtoken')[0]
+    from google.oauth2 import id_token
+    from google.auth.transport import requests
+    try:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), GOOGLE_CLIENT_ID)
+        userid = idinfo['sub']
+    except ValueError:
+        # Invalid token
+        print('flase')
+        return False
+    if not idinfo['aud'] == GOOGLE_CLIENT_ID:
+        flash('Inauthentic Google Sign In')
+        return (False, redirect('register'))
+    else:
+        return (True, idinfo)
+
+
+@app.route('/google_login', methods=['POST'])
+def google_login():
+    token = request.form.getlist('idtoken')[0]
+    from google.oauth2 import id_token
+    from google.auth.transport import requests
+    try:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), GOOGLE_CLIENT_ID)
+        userid = idinfo['sub']
+    except ValueError:
+        # Invalid token
+        print('flase')
+        return False
+    print(idinfo)
+    if not idinfo['aud'] == GOOGLE_CLIENT_ID:
+        flash('Inauthentic Google Sign In')
+    else:
+        flash('google sign in good')
+    return redirect(url_for('register'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('auth/login.html')
-    email = request.form.get('email')
-    username = request.form.get('username')
+        return render_template('auth/login.html',
+                                nav_buttons=login_buttons,
+                                GOOGLE_CLIENT_ID=GOOGLE_CLIENT_ID)
+    # POST request
+    username_or_email = request.form.get('userNameOrEmail')
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
-    user = User.search({'email': email})
+    if validate_email(username_or_email):
+        user = User.search({'email': username_or_email})
+    else:
+        user = User.search({'username': username_or_email})
     user = user[0] if user else None
-    if not user or not check_password_hash(user.password, password):
+    if not user:
+        flash('Incorrect email or username.')
+        return redirect(url_for('login'))
+    if not check_password_hash(user.password, password):
+        flash('Incorrect password.')
         return redirect(url_for('login'))
     login_user(user)
-    return redirect(url_for('profile'))
+    return redirect(url_for('profile'), remember=remember)
 
 
 @app.route('/logout')
@@ -139,7 +230,7 @@ def logout():
 Helper functions.
 ----------------------------
 """
-def bad_uploaded_file(request):
+def bad_uploaded_file(request, file=None):
     image_formats = ['png', 'jpg', 'jpeg', 'bmp']
     if 'file' not in request.files:
         print(1)
@@ -178,7 +269,7 @@ def password_check(password):
     # searching for lowercase
     lowercase_error = re.search(r"[a-z]", password) is None
     # searching for symbols
-    symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
+    symbol_error = re.search(r"\W", password) is None
     # overall result
     password_ok = not ( length_error or digit_error or uppercase_error or lowercase_error or symbol_error )
     return (password_ok,
@@ -191,6 +282,28 @@ def password_check(password):
             'a symbol.' : symbol_error,
             })
 
+def username_check(password):
+    """
+    Verify format of 'username'
+    Returns a dict indicating the incorrect criteria
+    A username is considered acceptable if:
+        33 characters length or more
+        no symbols or spaces
+    """
+    import re
+    # checking the length
+    length_error = len(password) > 33
+    # searching for symbols
+    symbol_error = re.search(r"\W", password)
+    # overall result
+    password_ok = not ( length_error or symbol_error )
+    return (password_ok,
+            {
+            'Username must: be unique, ': 1,
+            'be 33 characters or less, ' : length_error,
+            'contain only text and/or numbers.' : symbol_error,
+            })
+
 def save_profile_picture(file, user):
     if file and allowed_file(file.filename):
         ext = secure_filename(file.filename).split('.')[-1:][0].lower()
@@ -200,39 +313,16 @@ def save_profile_picture(file, user):
         user.save_to_db()
 
 
-@app.route('/tokensignin', methods=['POST'], strict_slashes=False)
-def google_signin_token():
-    token = request.form.getlist('idtoken')[0]
-    from google.oauth2 import id_token
-    from google.auth.transport import requests
-    # (Receive token by HTTPS POST)
-    # ...
-    try:
-        
-        # Specify the CLIENT_ID of the app that accesses the backend:
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
 
-        # Or, if multiple clients access the backend server:
-        # idinfo = id_token.verify_oauth2_token(token, requests.Request())
-        # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
-        #     raise ValueError('Could not verify audience.')
-
-        # If auth request is from a G Suite domain:
-        # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
-        #     raise ValueError('Wrong hosted domain.')
-
-        # ID token is valid. Get the user's Google Account ID from the decoded token.
-        userid = idinfo['sub']
-        print('userid = ', userid)
-    except ValueError:
-        # Invalid token
-        print('invalid')
-        pass
-    return redirect('/users')
-
-
-
-
+"""
+print('userid = ', userid)
+        print('given_name', idinfo['given_name'])
+        print('family_name', idinfo['family_name'])
+        print('email', idinfo['email'])
+        print('name', idinfo['name'])
+        print('picture', idinfo['picture'])
+        print('all', idinfo)
+        """
 
 
 
